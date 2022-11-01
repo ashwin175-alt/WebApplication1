77d303cf8558a9a7599a521c10b45f363d450540
Ashwin upadhyay <arush85cool@gmail.com>
2022-11-02 02:22:31 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        DAL dal = new DAL();
+        // GET: UserController
+        public ActionResult Index()
+        {
+            ModelState.Clear();
+            return View(dal.GetDataList());
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(dal.GetDataList().Find(ur=>ur.id == id));
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserRegModel ur)
+        {
+            try
+            {
+                if(dal.InsertData(ur))
+                {
+                    ViewBag.Message = "Data Saved";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(dal.GetDataList().Find(ur => ur.id == id));
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, UserRegModel ur)
+        {
+            try
+            {
+                if (dal.UpdateData(ur))
+                {
+                    ViewBag.Message = "Data Updated";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(dal.GetDataList().Find(ur => ur.id == id));
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, UserRegModel ur)
+        {
+            try
+            {
+                if (dal.DeleteData(ur))
+                {
+                    ViewBag.Message = "Data Deleted";
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
